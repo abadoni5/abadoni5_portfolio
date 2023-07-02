6ddac974512d0ca5bf29202c079f1d6dc12f174e
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2023-07-02 21:50:25 +0530

    bug fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Aayush &nbsp;
-            <span className='sm:block hidden'> | Badoni</span>
+            <span className='sm:block hidden'>Badoni</span>
           </p>
         </Link>
 
